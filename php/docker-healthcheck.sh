336343249ffb3cc11be8e751be34950c7bd76e58
kez <kerrialbeckettnewham@gmail.com>
2025-10-28 20:22:08 +0100

    switch from UNIX socket to TCP

--- php/docker-healthcheck.sh
+++ php/docker-healthcheck.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -e
 
-if env -i REQUEST_METHOD=GET SCRIPT_NAME=/ping SCRIPT_FILENAME=/ping cgi-fcgi -bind -connect /var/run/php/php-fpm.sock; then
-	exit 0
+if env -i REQUEST_METHOD=GET SCRIPT_NAME=/ping SCRIPT_FILENAME=/ping \
+    cgi-fcgi -bind -connect 127.0.0.1:9000 >/dev/null 2>&1; then
+  exit 0
 fi
 
 exit 1
